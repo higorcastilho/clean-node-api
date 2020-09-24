cab2bcaec41597e9eff0cfbcf2dfd5a8b3cbc8ff
Higor Castilho <castilhohf@gmail.com>
2020-09-24 12:37:52 -0300

    refactor: insert FACTORY DESIGN PATTERN - move sut creation to a factory helper method

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -2,9 +2,16 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
+//introduce the Factory Design Pattern. Basically avoid us to broke
+//all the snippets who call that instance of the class when changing
+//this instance
+const makeSut = () => {
+	return new LoginRouter()
+}
+
 describe('Login Router', () => {
 	test('Should return 400 if no email is provided', () => {
-		const sut = new LoginRouter()
+		const sut = makeSut()
 		const httpRequest = {
 			body: {
 				password: 'any_password'
@@ -16,7 +23,7 @@ describe('Login Router', () => {
 	})
 
 	test('Should return 400 if no password is provided', () => {
-		const sut = new LoginRouter()
+		const sut = makeSut()
 		const httpRequest = {
 			body: {
 				email: 'any_email@email.com'
@@ -28,14 +35,14 @@ describe('Login Router', () => {
 	})
 
 	test('Should return 500 if no httpRequest is provided', () => {
-		const sut = new LoginRouter()
+		const sut = makeSut()
 		const httpResponse = sut.route()
 		expect(httpResponse.statusCode).toBe(500)	
 			
 	})
 
 	test('Should return 500 if httpRequest has no body', () => {
-		const sut = new LoginRouter()
+		const sut = makeSut()
 		const httpRequest = {}
 		const httpResponse = sut.route(httpRequest)
 		expect(httpResponse.statusCode).toBe(500)	
